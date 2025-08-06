b3932cd7df4ec341ce2aa40ad2547566e9ebacbc
Shinroi1 <altoverossam@gmail.com>
2025-08-06 22:12:50 +0800

    Fix: specify app name in makemigrations

--- run_migrate.py
+++ run_migrate.py
@@ -7,5 +7,5 @@ django.setup()
 
 from django.core.management import call_command
 
-call_command('makemigrations')
-call_command("migrate")
+call_command('makemigrations', 'myapp')  # 👈 Explicitly name your app
+call_command('migrate')
