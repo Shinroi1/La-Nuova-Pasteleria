92dc7c28cd080f68149ae516f04b568f80981076
Shinroi1 <altoverossam@gmail.com>
2025-08-06 21:36:56 +0800

    Temporary: run migration on Railway

---
+++ run_migrate.py
@@ -0,0 +1,10 @@
+# run_migrate.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+django.setup()
+
+from django.core.management import call_command
+
+call_command("migrate")
